47d7aeee116a8746dacf0b556eb3385290c9b217
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2019-02-28 16:50:54 +0100

    Threding threads

--- run.py
+++ run.py
@@ -1,14 +1,39 @@
 #!/usr/bin/env python3
 import argparse
+import multiprocessing
+import random
+import time
+from threading import Thread, Lock
+
 import myio
 
+lock = Lock()
+global_variable_yeha = 10
 
-def main(input: str, output: str):
-	lines = myio.read(input)
 
+def compute_it(id):
 	# COMPUTE IT
+	global global_variable_yeha
 
-	myio.write(output, lines)
+	lock.acquire()
+	print(f"{id} in da' lock: {global_variable_yeha}")
+	global_variable_yeha += 1
+	lock.release()
+
+
+def main(input_file: str, output_file: str):
+	lines = myio.read(input_file)
+
+	workers = []
+	for i in range(0, multiprocessing.cpu_count()):
+		workers.append(Thread(target=compute_it, args=(str(i))))
+		workers[-1].start()
+
+	for worker in workers:
+		# Workers of the world, unite!
+		worker.join()
+
+	myio.write(output_file, lines)
 
 
 if __name__ == "__main__":
