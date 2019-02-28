a045381dfbe05704ed93320f74a6075214832485
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2019-02-28 16:33:07 +0100

    Argparse

--- run.py
+++ run.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
-
+import argparse
 import myio
 
 
-def main():
-	lines = myio.read('input.txt')
+def main(input: str, output: str):
+	lines = myio.read(input)
 
 	# COMPUTE IT
 
-	myio.write('output.txt', lines)
+	myio.write(output, lines)
 
 
 if __name__ == "__main__":
-	main()
+	parser = argparse.ArgumentParser(description='Do awesome computations, yeha.')
+	parser.add_argument('input', type=str, help="Input file name")
+	parser.add_argument('output', type=str, help="Output file name")
+
+	args = parser.parse_args()
+
+	main(args.input, args.output)
