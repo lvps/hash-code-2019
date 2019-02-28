676a9cb2baedd4006395aefd88dac05bf6c12cdd
Ludovico Pavesi <ludovico.pavesi@gmail.com>
2019-02-28 20:27:40 +0100

    Added Massa and Carrara

---
+++ massa.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from subprocess import call
+
+call(['./run.py', 'a_example.txt', 'out_a_example.txt'])
+call(['./run.py', 'b_lovely_landscapes.txt', 'out_b_lovely_landscapes.txt'])
+call(['./run.py', 'c_memorable_moments.txt', 'out_c_memorable_moments.txt'])
+call(['./run.py', 'd_pet_pictures.txt', 'out_d_pet_pictures.txt'])
+call(['./run.py', 'e_shiny_selfies.txt', 'out_e_shiny_selfies.txt'])
